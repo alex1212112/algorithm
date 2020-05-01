62c731814b6e6d6e5a5d5ecbea6e42ecda20e86b
renguohua <renguohua@didichuxing.com>
2020-05-01 17:38:16 +0800

    eslint

--- leetcode/88/index.js
+++ leetcode/88/index.js
@@ -24,45 +24,45 @@
 var merge = function (nums1, m, nums2, n) {
   if (m == 0) {
     for (let index = 0; index < n; index++) {
-      nums1[index] = nums2[index];
+      nums1[index] = nums2[index]
     }
-    return;
+    return
   }
-  let num1Index = 0;
-  let num2Index = 0;
-  let num1Tag = 0;
+  let num1Index = 0
+  let num2Index = 0
+  let num1Tag = 0
   while (num2Index < n) {
     while (num1Index < n + m && num1Tag < m) {
       // console.log(nums2[num2Index], nums1[num1Index]);
       if (nums2[num2Index] <= nums1[num1Index]) {
         for (let index = m + n - 1; index > num1Index; index--) {
-          nums1[index] = nums1[index - 1];
+          nums1[index] = nums1[index - 1]
         }
         // 插入
-        nums1[num1Index] = nums2[num2Index];
-        num1Index++;
-        break;
+        nums1[num1Index] = nums2[num2Index]
+        num1Index++
+        break
       } else {
-        num1Index++;
-        num1Tag++;
+        num1Index++
+        num1Tag++
       }
     }
     // console.log(num2Index);
-    
+
     if (num1Tag == m) {
       // console.log('tag' + num1Tag);
-      break;
+      break
     }
-    num2Index++;
+    num2Index++
   }
   // num2Index--;
   while (num2Index < n) {
-    nums1[m + num2Index] = nums2[num2Index];
+    nums1[m + num2Index] = nums2[num2Index]
     num2Index++
   }
-};
+}
 
-const nums1 = [2,0];
-const nums2 = [1];
-merge(nums1, 1, nums2, 1);
-console.log(nums1);
+const nums1 = [2, 0]
+const nums2 = [1]
+merge(nums1, 1, nums2, 1)
+console.log(nums1)
