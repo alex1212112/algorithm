1c1cfc9c7a9112c6894fae8d9635722ceae10e29
alex1212112 <alex1212112@gmail.com>
2020-06-02 17:42:47 +0800

    Not a good solution for 5

---
+++ leetcode/5/index.js
@@ -0,0 +1,62 @@
+// Longest Palindromic Substring
+
+// Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+// Example 1:
+
+// Input: "babad"
+// Output: "bab"
+// Note: "aba" is also a valid answer.
+// Example 2:
+
+// Input: "cbbd"
+// Output: "bb"
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  const subs = substrings(s)
+  for (let sub of subs) {
+    if (isPalindrome(sub)) {
+      return sub
+    }
+  }
+  return ''
+}
+
+/**
+ * @param {string} s
+ * @return {bool}
+ */
+function isPalindrome(s) {
+  if (s.length <= 1) {
+    return true
+  }
+  const left = s.substring(0, Math.floor(s.length / 2))
+  const right = s.substring(Math.ceil(s.length / 2))
+  return left == right.split('').reverse().join('')
+}
+
+/**
+ * @param {string} s
+ * @return {Array}
+ */
+function substrings(s) {
+  const array = []
+  for (let count = s.length; count > 0; count--) {
+    for (let index = 0; index <= s.length - count; index++) {
+      let sub = s.substring(index, index + count)
+      console.log(sub)
+      array.push(sub)
+    }
+  }
+  return array
+}
+
+const s =
+  'jrjnbctoqgzimtoklkxcknwmhiztomaofwwzjnhrijwkgmwwuazcowskjhitejnvtblqyepxispasrgvgzqlvrmvhxusiqqzzibcyhpnruhrgbzsmlsuacwptmzxuewnjzmwxbdzqyvsjzxiecsnkdibudtvthzlizralpaowsbakzconeuwwpsqynaxqmgngzpovauxsqgypinywwtmekzhhlzaeatbzryreuttgwfqmmpeywtvpssznkwhzuqewuqtfuflttjcxrhwexvtxjihunpywerkktbvlsyomkxuwrqqmbmzjbfytdddnkasmdyukawrzrnhdmaefzltddipcrhuchvdcoegamlfifzistnplqabtazunlelslicrkuuhosoyduhootlwsbtxautewkvnvlbtixkmxhngidxecehslqjpcdrtlqswmyghmwlttjecvbueswsixoxmymcepbmuwtzanmvujmalyghzkvtoxynyusbpzpolaplsgrunpfgdbbtvtkahqmmlbxzcfznvhxsiytlsxmmtqiudyjlnbkzvtbqdsknsrknsykqzucevgmmcoanilsyyklpbxqosoquolvytefhvozwtwcrmbnyijbammlzrgalrymyfpysbqpjwzirsfknnyseiujadovngogvptphuyzkrwgjqwdhtvgxnmxuheofplizpxijfytfabx'
+const result = longestPalindrome(s)
+
+console.log(`${result}`)
