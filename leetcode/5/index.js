2e932042e545452f5c06def203b0ae98261d0cd8
alex1212112 <alex1212112@gmail.com>
2020-06-03 23:10:48 +0800

    center point

--- leetcode/5/index.js
+++ leetcode/5/index.js
@@ -17,73 +17,46 @@
  * @return {string}
  */
 var longestPalindrome = function (s) {
-  const rs = s.split('').reverse().join('')
-  return longestSameSubstring(s, rs)
-}
-
-/**
- * @param {string} s1
- * @param {string} s2
- * @return {string}
- */
-function longestSameSubstring(s1, s2) {
-  const m = s1.length
-  const n = s2.length
-  const dp = [new Array(n + 1).fill(0)]
+  const len = s.length
   let longest = 0
-  let x = 0
-  let y = 0
-  for (let i = 1; i <= m; i++) {
-    dp[i] = [0]
-    for (let j = 1; j <= n; j++) {
-      if (s1[i - 1] === s2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1
-        if (dp[i][j] > longest) {
-          let tempx = i - 1
-          let tempy = j - 1
-          const len = tempx + tempy + 2 - s1.length
-          if (len > 0) {
-            const sub1 = s1.substring(tempx + 1 - len, tempx + 1)
-            const sub2 = s2.substring(tempy + 1 - len, tempy + 1)
-            console.log(sub1)
-            console.log('\n')
-            console.log(sub2)
-            console.log(len)
-            console.log('------------------')
-            if (sub1 == sub2) {
-              longest = Math.max(longest, dp[i][j])
-              x = tempx
-              y = tempy
-              console.log(`x: ${x}`)
-              console.log(`y: ${y}`)
-              console.log(`x+1: ${s1[x+1]}`)
-              console.log(`y+1: ${s2[y+1]}`)
-              // console.log(`sub1: ${sub1}`)
-              // console.log(`sub2: ${sub2}`)
-              console.log(`t: ${s2.length}`)
-              console.log(`longest: ${longest}`)
-              console.log(`----------`)
-            }
-          }
-        }
-      } else {
-        dp[i][j] = 0
-      }
-    }
-  }
-
   let result = ''
-  for (let index = 0; index < longest; index++) {
-    result = `${s1[x]}${result}`
-    x--
-    y--
+  for (let index = 0; index < len; index++) {
+    let c = 0
+    let now = 0
+    for (let j = 0; index - j >= 0 && index + j < len; j++) {
+      if (s[index - j] != s[index + j]) {
+        break
+      }
+      c = j * 2 + 1
+      now = j
+    }
+    if (longest < c) {
+      longest = c
+      result = s.substring(index - now, index + now + 1)
+    }
+    for (let j = 0; index - j >= 0 && index + j + 1 < len; j++) {
+      if (s[index - j] != s[index + j + 1]) {
+        break
+      }
+      c = j * 2 + 2
+      now = j
+    }
+    if (longest < c) {
+      longest = c
+      result = s.substring(index - now, index + now + 2)
+    }
   }
   return result
 }
 
+function isPalindrome(s) {
+  return s.split('').reverse().join('') == s
+}
+
+// const s =
+// 'jrjnbctoqgzimtoklkxcknwmhiztomaofwwzjnhrijwkgmwwuazcowskjhitejnvtblqyepxispasrgvgzqlvrmvhxusiqqzzibcyhpnruhrgbzsmlsuacwptmzxuewnjzmwxbdzqyvsjzxiecsnkdibudtvthzlizralpaowsbakzconeuwwpsqynaxqmgngzpovauxsqgypinywwtmekzhhlzaeatbzryreuttgwfqmmpeywtvpssznkwhzuqewuqtfuflttjcxrhwexvtxjihunpywerkktbvlsyomkxuwrqqmbmzjbfytdddnkasmdyukawrzrnhdmaefzltddipcrhuchvdcoegamlfifzistnplqabtazunlelslicrkuuhosoyduhootlwsbtxautewkvnvlbtixkmxhngidxecehslqjpcdrtlqswmyghmwlttjecvbueswsixoxmymcepbmuwtzanmvujmalyghzkvtoxynyusbpzpolaplsgrunpfgdbbtvtkahqmmlbxzcfznvhxsiytlsxmmtqiudyjlnbkzvtbqdsknsrknsykqzucevgmmcoanilsyyklpbxqosoquolvytefhvozwtwcrmbnyijbammlzrgalrymyfpysbqpjwzirsfknnyseiujadovngogvptphuyzkrwgjqwdhtvgxnmxuheofplizpxijfytfabx'
 const s =
-  'jrjnbctoqgzimtoklkxcknwmhiztomaofwwzjnhrijwkgmwwuazcowskjhitejnvtblqyepxispasrgvgzqlvrmvhxusiqqzzibcyhpnruhrgbzsmlsuacwptmzxuewnjzmwxbdzqyvsjzxiecsnkdibudtvthzlizralpaowsbakzconeuwwpsqynaxqmgngzpovauxsqgypinywwtmekzhhlzaeatbzryreuttgwfqmmpeywtvpssznkwhzuqewuqtfuflttjcxrhwexvtxjihunpywerkktbvlsyomkxuwrqqmbmzjbfytdddnkasmdyukawrzrnhdmaefzltddipcrhuchvdcoegamlfifzistnplqabtazunlelslicrkuuhosoyduhootlwsbtxautewkvnvlbtixkmxhngidxecehslqjpcdrtlqswmyghmwlttjecvbueswsixoxmymcepbmuwtzanmvujmalyghzkvtoxynyusbpzpolaplsgrunpfgdbbtvtkahqmmlbxzcfznvhxsiytlsxmmtqiudyjlnbkzvtbqdsknsrknsykqzucevgmmcoanilsyyklpbxqosoquolvytefhvozwtwcrmbnyijbammlzrgalrymyfpysbqpjwzirsfknnyseiujadovngogvptphuyzkrwgjqwdhtvgxnmxuheofplizpxijfytfabx'
-// const s = 'abcdefgfexyz'
+  'aaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaa'
 const result = longestPalindrome(s)
 
 console.log(`${result}`)
