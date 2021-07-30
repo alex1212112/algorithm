d27c64335adf8a31468521354ee7ed77b4fada09
alex1212112 <alex1212112@gmail.com>
2021-07-30 18:47:51 +0800

    string contains

---
+++ playground/string_contains/index.js
@@ -0,0 +1,56 @@
+// 字符串包含
+
+// 题目描述
+
+// 给定两个分别由字母组成的字符串A和字符串B，字符串B的长度比字符串A短。请问，如何最快地判断字符串B中所有字母是否都在字符串A里？
+
+// 为了简单起见，我们规定输入的字符串只包含大写英文字母，请实现函数bool stringContains(string a, string b)
+
+// 比如，如果是下面两个字符串：
+
+// String 1：ABCD
+
+// String 2：BAD
+
+// 答案是true，即String2里的字母在String1里也都有，或者说String2是String1的真子集。
+
+// 如果是下面两个字符串：
+
+// String 1：ABCD
+
+// String 2：BCE
+
+// 答案是false，因为字符串String2里的E字母不在字符串String1里。
+
+// 同时，如果string1：ABCD，string 2：AA，同样返回true。
+
+
+function stringContains(a, b) {
+
+  const list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,61, 67, 71, 73, 79, 83, 89, 97, 101]
+  let sum = 1
+
+  for(let i = 0; i < a.length; i++ ) {
+    const index = a.charCodeAt(i) - 'A'.charCodeAt(0);
+    sum = sum * list[index]
+  }
+  
+  for(let i = 0; i < b.length; i++) {
+
+    const index = b.charCodeAt(i) - 'A'.charCodeAt(0);
+    const num = list[index];
+
+    if(sum % num != 0) {
+      return false
+    }
+  }
+  return true;
+}
+
+
+const a = 'ABCDEFG'
+const b = 'BCD'
+
+const result = stringContains(a, b)
+
+console.log(`result is ${result}`)
