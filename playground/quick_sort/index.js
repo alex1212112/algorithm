d981e3271f3b30a3dd36257f0b2d256bbd47a39b
alex1212112 <alex1212112@gmail.com>
2021-08-28 20:03:35 +0800

    quick sort by js

---
+++ playground/quick_sort/index.js
@@ -0,0 +1,63 @@
+function quikSort(list) {
+  quikSortHelp(list, 0, list.length - 1)
+  return list
+}
+
+function quikSortHelp(list, low, high) {
+  const p = findPostion(list, low, high)
+  if(p - 1 > low) {
+    quikSortHelp(list, low, p - 1) 
+  }
+  if(p + 1 < high) {
+    quikSortHelp(list, p + 1, high) 
+  }
+  return list
+}
+
+function findPostion(list, low, high) {
+  let postion = low
+  let currentIndex = low
+  const tmp = list[low]
+  for(let index=low + 1; index<=high; index++) {
+    if(list[index] < tmp) {
+      swap(list, postion, index)
+      postion++ 
+      if(list[index] == tmp) {
+        currentIndex = index
+      }
+    }
+  }
+  swap(list, postion, currentIndex)
+  return postion
+}
+
+function swap(list, p1, p2) {
+  if(p1 == p2) {
+    return
+  }
+  let tmp = list[p1]
+  list[p1] = list[p2]
+  list[p2] = tmp
+}
+
+function generateList(n) {
+  const list = []
+  for(let index=0; index<n; index++) {
+    const number = Math.floor(Math.random() * 100)
+    const signedNumber = Math.random() > 0.5 ? number : -number
+    list.push(signedNumber)
+  }
+  return list
+}
+
+
+// const list = [ 8, 7, -3, 11, 421, -23, 98, -25, 24]
+// const result = quikSort(list)
+// console.log(result)
+
+const list = generateList(100)
+console.log(list)
+const result = quikSort(list)
+console.log(result)
+
+
