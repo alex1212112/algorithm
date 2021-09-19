f9050c21b90d26240756fcad1a05e0c1f232221d
alex1212112 <alex1212112@gmail.com>
2021-09-19 18:46:03 +0800

    309 Best Time to Buy and Sell Stock with Cooldown

--- leetcode/309 Best Time to Buy and Sell Stock with Cooldown/index.js
+++ leetcode/309 Best Time to Buy and Sell Stock with Cooldown/index.js
@@ -36,8 +36,8 @@ var maxProfit = function(prices) {
       dp[i][0] = 0
       dp[i][1] = -prices[i]
     } else if( i == 1) {
-      dp[i][0] = 0
-      dp[i][1] = -prices[i]
+      dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i])
+      dp[i][1] = Math.max(dp[i-1][1], -prices[i])
     } else {
       dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i])
       dp[i][1] = Math.max(dp[i-1][1], dp[i-2][0] -prices[i])
