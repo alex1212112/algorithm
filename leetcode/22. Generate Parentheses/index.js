0c1cd27c2b53ea2247f25f368e8f134444186731
alex1212112 <alex1212112@gmail.com>
2023-03-22 00:33:40 +0800

    22. Generate Parentheses

---
+++ leetcode/22. Generate Parentheses/index.js
@@ -0,0 +1,96 @@
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+// Example 2:
+
+// Input: n = 1
+// Output: ["()"]
+ 
+
+// Constraints:
+
+// 1 <= n <= 8
+
+
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+
+    const result = []
+    const options = ['(', ')']
+
+    backtrack('', options, result, n, n, n)
+    return result
+
+};
+
+
+function backtrack(path, options, result, leftCount, rightCount, pairCount) {
+
+    if(path.length == pairCount * 2) {
+        result.push(path)
+        return
+    }
+
+    for(let index = 0; index < options.length; index++) {
+
+        const item = options[index]         
+
+        if(item == '(' && isNextLeftValid(path, pairCount)) {
+            const tmpPath = path
+            path = `${path}(`
+            backtrack(path, options, result, leftCount - 1, rightCount, pairCount)
+            path = tmpPath
+        } else if( item == ')' && isNextRightValid(path)) {
+            const tmpPath = path
+            path = `${path})`
+            backtrack(path, options, result, leftCount , rightCount - 1, pairCount)
+            path = tmpPath
+        }
+    }
+}
+
+
+function isNextRightValid(path) {
+    const leftCount = leftCountFromPath(path)
+    const righCount = rightCountFromPath(path)
+    return leftCount > righCount
+}
+
+function isNextLeftValid(path, count) {
+    const leftCount = leftCountFromPath(path) 
+    return leftCount < count
+}
+
+
+function leftCountFromPath(path) {
+    let count = 0
+    for(let str of path) {
+        if(str == '(') {
+            count++
+        }
+    }
+    return count
+}
+
+function rightCountFromPath(path) {
+    let count = 0
+    for(let str of path) {
+        if(str == ')') {
+            count++
+        }
+    }
+    return count
+}
+
+
+const result = generateParenthesis(4)
+console.log(result)
