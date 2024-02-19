f5fd8c849f9bff0888857dc90059b5bfe718fb07
alex1212112 <alex1212112@gmail.com>
2024-02-19 10:17:46 +0800

    110. Balanced Binary Tree

---
+++ leetcode/110. Balanced Binary Tree/index.js
@@ -0,0 +1,57 @@
+
+// Given a binary tree, determine if it is 
+// height-balanced
+// .
+
+ 
+
+// Example 1:
+
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: true
+// Example 2:
+
+
+// Input: root = [1,2,2,3,3,null,null,4,4]
+// Output: false
+// Example 3:
+
+// Input: root = []
+// Output: true
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 5000].
+// -104 <= Node.val <= 104
+
+
+ function TreeNode(val, left, right) {
+     this.val = (val===undefined ? 0 : val)
+     this.left = (left===undefined ? null : left)
+     this.right = (right===undefined ? null : right)
+ }
+ 
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+  if(!root) {
+    return true
+  }
+  const is = Math.abs(height(root.left) - height(root.right)) <= 1
+  return is && isBalanced(root.left) && isBalanced(root.right)
+};
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function height(root) {
+  if(!root) {
+    return 0
+  }
+  return Math.max(height(root.left), height(root.right)) + 1
+}
