85b78237beb7b4da56fbad2b10fedfcb5f5b2aae
alex1212112 <alex1212112@gmail.com>
2024-05-30 11:17:48 +0800

    Edit Distance

--- leetcode/72. Edit Distance/index.js
+++ leetcode/72. Edit Distance/index.js
@@ -210,6 +210,33 @@ function minDistance3(s, t) {
   return dp[m];
 }
 
+function minDistance4(s, t) {
+  const n = s.length,
+      m = t.length;
+  const dp = Array.from({ length: n + 1 }, () => new Array(m + 1).fill(0));
+  // 状态转移：首行首列
+  for (let i = 1; i <= n; i++) {
+      dp[i][0] = i;
+  }
+  for (let j = 1; j <= m; j++) {
+      dp[0][j] = j;
+  }
+  // 状态转移：其余行和列
+  for (let i = 1; i <= n; i++) {
+      for (let j = 1; j <= m; j++) {
+          if (s.charAt(i - 1) === t.charAt(j - 1)) {
+              // 若两字符相等，则直接跳过此两字符
+              dp[i][j] = dp[i - 1][j - 1];
+          } else {
+              // 最少编辑步数 = 插入、删除、替换这三种操作的最少编辑步数 + 1
+              dp[i][j] =
+                  Math.min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1;
+          }
+      }
+  }
+  return dp[n][m];
+}
+
 
 const string1 = 'intention'
 const string2 = 'execution'
