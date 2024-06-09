7d40066f23abd4c3c21e754bddb4cd12f4340731
alex1212112 <alex1212112@gmail.com>
2024-06-09 18:15:36 +0800

    173. Binary Search Tree Iterator

---
+++ leetcode/173. Binary Search Tree Iterator/index.js
@@ -0,0 +1,95 @@
+// Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+// BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+// boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+// int next() Moves the pointer to the right, then returns the number at the pointer.
+// Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+// You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+ 
+
+// Example 1:
+
+
+// Input
+// ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+// [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+// Output
+// [null, 3, 7, true, 9, true, 15, true, 20, false]
+
+// Explanation
+// BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+// bSTIterator.next();    // return 3
+// bSTIterator.next();    // return 7
+// bSTIterator.hasNext(); // return True
+// bSTIterator.next();    // return 9
+// bSTIterator.hasNext(); // return True
+// bSTIterator.next();    // return 15
+// bSTIterator.hasNext(); // return True
+// bSTIterator.next();    // return 20
+// bSTIterator.hasNext(); // return False
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 105].
+// 0 <= Node.val <= 106
+// At most 105 calls will be made to hasNext, and next.
+ 
+
+// Follow up:
+
+// Could you implement next() and hasNext() to run in average O(1) time and use O(h) memory, where h is the height of the tree?
+
+
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+/**
+ * @param {TreeNode} root
+ */
+var BSTIterator = function(root) {
+   this.root = root 
+   this.list = []
+   this.traversal(root)
+   this.tag = 0
+};
+
+/**
+ * @return {number}
+ */
+BSTIterator.prototype.next = function() {
+    if(!this.hasNext()) {
+      return null 
+    }
+    this.tag++
+    return this.list[this.tag - 1]
+};
+
+/**
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function() {
+    return this.tag < this.list.length
+};
+
+/** 
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
+
+BSTIterator.prototype.traversal = function(node) {
+  if(!node) {
+    return
+  }
+  this.traversal(node.left)
+  this.list.push(node.val)
+  this.traversal(node.right)
+}
