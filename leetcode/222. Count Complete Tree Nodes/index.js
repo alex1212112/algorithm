8e6e9e8f25a83e20ca25ddbdbe1beb0958803f13
alex1212112 <alex1212112@gmail.com>
2024-06-09 19:51:25 +0800

    222. Count Complete Tree Nodes

---
+++ leetcode/222. Count Complete Tree Nodes/index.js
@@ -0,0 +1,68 @@
+// Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+// According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+// Design an algorithm that runs in less than O(n) time complexity.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,3,4,5,6]
+// Output: 6
+// Example 2:
+
+// Input: root = []
+// Output: 0
+// Example 3:
+
+// Input: root = [1]
+// Output: 1
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 5 * 104].
+// 0 <= Node.val <= 5 * 104
+// The tree is guaranteed to be complete.
+
+
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+  if(!root) {
+    return 0
+  }
+  const queue = [root]
+  const count = bfs(queue)
+  return count    
+};
+
+/**
+ * @param {number[]} queue
+ * @return {number}
+ */
+function bfs(queue) {
+  let count = 0
+  while(queue.length > 0) {
+    const root = queue.shift()
+    if(root.left) {
+      queue.push(root.left)
+    }
+    if(root.right) {
+      queue.push(root.right)
+    }
+    count++
+  }
+  return count
+}
